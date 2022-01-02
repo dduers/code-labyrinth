0bc5319efd6c94f81ab4cd1e5588939b1db9f4dd
Daniel Duersteler <daniel.duersteler@gmail.com>
2022-01-02 03:52:33 +0100

    updated events

--- public/js/app.js
+++ public/js/app.js
@@ -37,6 +37,31 @@ window.addEventListener('load', () => {
     window.addEventListener('resize', resize);
 });
 
+var isEventSupported = (function() {
+    var TAGNAMES = {
+        'select': 'input',
+        'change': 'input',
+        'submit': 'form',
+        'reset': 'form',
+        'error': 'img',
+        'load': 'img',
+        'abort': 'img'
+    }
+
+    function isEventSupported(eventName) {
+        var el = document.createElement(TAGNAMES[eventName] || 'div');
+        eventName = 'on' + eventName;
+        var isSupported = (eventName in el);
+        if (!isSupported) {
+            el.setAttribute(eventName, 'return;');
+            isSupported = typeof el[eventName] == 'function';
+        }
+        el = null;
+        return isSupported;
+    }
+    return isEventSupported;
+})();
+
 function resize() {
     let size = Math.min(window.innerWidth, window.innerHeight);
     ctx.canvas.width = size;
